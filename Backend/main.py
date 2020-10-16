feb0c7a07950ba9a0472a666b6421e166160b8ca
Jraiban <38603614+Jraiban@users.noreply.github.com>
2020-10-16 20:45:19 +0200

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -1,28 +1,158 @@
 import json,os
 from flask import Flask,json
-from flask_restful import Api, Resource
+from flask_cors import CORS
+from flask import request
+from flask import jsonify, make_response
+
+
 
 app = Flask(__name__)
-api =  Api(app)
+CORS(app)
+
+        
+@app.route('/Rest',methods=['GET'])
+def get_temperature():
+    # open file Json
+    with open('names.json', 'r') as myfile:
+        data=myfile.read()
+        obj = json.loads(data)
+        print(obj["Rest"])
+    return str(obj["Rest"])
+
+    
+@app.route('/Vorrat',methods=['GET'])
+def get_temperature():
+    # open file Json
+    with open('names.json', 'r') as myfile:
+        data=myfile.read()
+        obj = json.loads(data)
+        print(obj["Vorrat"])
+    return str(obj["Vorrat"])
+
+ 
+    
+@app.route('/Abfallreduzierung',methods=['GET'])
+def get_temperature():
+    # open file Json
+    with open('names.json', 'r') as myfile:
+        data=myfile.read()
+        obj = json.loads(data)
+        print(obj["Abfallreduzierung"])
+    return str(obj["Abfallreduzierung"])
 
 
-# open file Json
-with open('names.json', 'r') as myfile:
-    data=myfile.read()
 
-# parse file 
-obj = json.loads(data)
+@app.route('/Wasserverbrauch',methods=['GET'])
+def get_temperature():
+    # open file Json
+    with open('names.json', 'r') as myfile:
+        data=myfile.read()
+        obj = json.loads(data)
+        print(obj["Wasserverbrauch"])
+    return str(obj["Wasserverbrauch"])
 
 
-class gettheobject(Resource):
-  def get(self, name):
-  
-    return obj[name]
+@app.route('/Hallenummer',methods=['GET'])
+def get_temperature():
+    # open file Json
+    with open('names.json', 'r') as myfile:
+        data=myfile.read()
+        obj = json.loads(data)
+        print(obj["Hallenummer"])
+    return str(obj["Hallenummer"])
+
+
+
+@app.route('/Mechankername',methods=['GET'])
+def get_temperature():
+    # open file Json
+    with open('names.json', 'r') as myfile:
+        data=myfile.read()
+        obj = json.loads(data)
+        print(obj["Mechankername"])
+    return str(obj["Mechankername"])
+
+ 
+
+@app.route('/Reparatur',methods=['GET'])
+def get_temperature():
+    # open file Json
+    with open('names.json', 'r') as myfile:
+        data=myfile.read()
+        obj = json.loads(data)
+        print(obj["Reparatur"])
+    return str(obj["Reparatur"])
 
  
 
 
-api.add_resource(gettheobject,"/op/<string:name>/")
+@app.route('/Energieverbrauch',methods=['GET'])
+def get_temperature():
+    # open file Json
+    with open('names.json', 'r') as myfile:
+        data=myfile.read()
+        obj = json.loads(data)
+        print(obj["Energieverbrauch"])
+    return str(obj["Energieverbrauch"])
+
+ 
+   
+@app.route('/Oelwechsel',methods=['GET'])
+def get_temperature():
+    # open file Json
+    with open('names.json', 'r') as myfile:
+        data=myfile.read()
+        obj = json.loads(data)
+        print(obj["Oelwechsel"])
+    return str(obj["Oelwechsel"])
+
+ 
+   
+   
+@app.route('/Defekt',methods=['GET'])
+def get_temperature():
+    # open file Json
+    with open('names.json', 'r') as myfile:
+        data=myfile.read()
+        obj = json.loads(data)
+        print(obj["Defekt"])
+    return str(obj["Defekt"])
+
+ 
+    
+@app.route('/Machinennummer',methods=['GET'])
+def get_temperature():
+    # open file Json
+    with open('names.json', 'r') as myfile:
+        data=myfile.read()
+        obj = json.loads(data)
+        print(obj["Machinennummer"])
+    return str(obj["Machinennummer"])
+
+
+
+@app.route('/temperatur',methods=['GET'])
+def get_temperature():
+    # open file Json
+    with open('names.json', 'r') as myfile:
+        data=myfile.read()
+        obj = json.loads(data)
+        print(obj["Temperatur"])
+    return str(obj["Temperatur"])
+
+@app.route('/wartungszeit',methods=['GET'])
+def get_wartungszeit():
+    # open file Json
+    with open('names.json', 'r') as myfile:
+        data=myfile.read()
+        obj = json.loads(data)
+        print(obj["Wartungszeit"])
+    return str(obj["Wartungszeit"])
+
+
+
+ 
+
 
 if __name__ == "__main__":
     app.run(debug=False) 
